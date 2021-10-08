9572b02df2c0ab04687a05fc5e397b1666c92f57
Ha Ngoc Linh <harnet.linh@gmail.com>
2021-10-08 21:15:30 +0700

    update

--- src/App.vue
+++ src/App.vue
@@ -2,17 +2,39 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Linh v-bind:msg_time="current_time"/>
   </div>
 </template>
 
 <script>
+import Linh from "./components/HelloLinh.vue"
 import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
+    HelloWorld,
+    Linh
+  },
+  created(){
+    this.getCurrentTime();
+  },
+  data(){
+    return {
+      name: "Linh",
+      current_time: ""
+    }
+  },
+  methods: {
+    getCurrentTime(){
+      fetch("http://worldtimeapi.org/api/ip").then(response => {
+        response.json().then(res => {
+          console.log(res);
+          this.current_time = res.datetime;
+        })
+      })
+    }
+  },
 }
 </script>
 
---
+++ src/components/HelloLinh.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <h1>
+        My new vue file
+    </h1>
+    <h2> {{msg_time}} </h2>
+  </div>
+</template>
+
+<script>
+export default {
+props: {
+    msg_time: String
+  },
+}
+</script>
+
+<style>
+
+</style>
